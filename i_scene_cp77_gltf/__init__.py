d8e22da30feddb0e8f852e9c6165e4563d24dead
dragonzkiller <dragonzkiller@gmail.com>
2022-06-13 19:40:39 -0700

    Initial streaming sector imports - requires wkit 8.7 nightly and some manual intervention

--- i_scene_cp77_gltf/__init__.py
+++ i_scene_cp77_gltf/__init__.py
@@ -11,8 +11,12 @@ bl_info = {
 
 import bpy
 import bpy.utils.previews
+import glob
 import json
 import os
+import os.path
+import platform
+
 
 from bpy.props import (
     StringProperty,
@@ -27,7 +31,6 @@ icons_dir = os.path.join(os.path.dirname(__file__), "icons")
 custom_icon_col = {}
 
 class CP77StreamingSectorImport(bpy.types.Operator,ImportHelper):
-
     bl_idname = "io_scene_glft.cp77sector"
     bl_label = "Import StreamingSector"
     use_filter_folder = True
@@ -35,9 +38,144 @@ class CP77StreamingSectorImport(bpy.types.Operator,ImportHelper):
         default=".",
         options={'HIDDEN'},
         )
+    import_materials: BoolProperty(name="Import Model Materials",default=True,description="Enable this option to include materials on the imported meshes")
+    image_format: EnumProperty(
+        name="Textures",
+        items=(("png", "Use PNG textures", ""),
+                ("dds", "Use DDS textures", ""),
+                ("jpg", "Use JPG textures", ""),
+                ("tga", "Use TGA textures", ""),
+                ("bmp", "Use BMP textures", ""),
+                ("jpeg", "Use JPEG textures", "")),
+        description="Texture Format",
+        default="png")
+    exclude_unused_mats: BoolProperty(name="Exclude Unused Materials",default=True,description="Enabling this options skips all the materials that aren't being used by any mesh") 
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.prop(self, 'import_materials')
+        layout.prop(self, 'image_format')
+        layout.prop(self, 'exclude_unused_mats')
 
     def execute(self, context):
-        self.report({'ERROR'}, "Streaming Sector Import is not yet implemented!")
+        path = self.properties.filepath
+
+        jsonpath = glob.glob(path+"\**\*.streamingsector.json", recursive = True)
+        filepath = jsonpath[0]
+
+        path2 = os.path.splitext(filepath)[0] + r'.worldNodeData.json'
+
+        with open(filepath,'r') as f: 
+              j=json.load(f) 
+      
+        with open(path2,'r') as f: 
+              t=json.load(f) 
+      
+        meshes =  glob.glob(path+"\**\*.glb", recursive = True)
+
+        glbnames = [ os.path.basename(x) for x in meshes]
+        meshnames = [ os.path.splitext(x)[0]+".mesh" for x in glbnames]
+
+        prop = j["Data"]["RootChunk"]["Properties"]
+        nodes = prop["nodes"]
+
+        for i,e in enumerate(nodes):
+            data = e['Data']["Properties"]
+            for k in (data.keys()):
+                if isinstance(data[k], dict) and ("DepotPath" in data[k].keys()):
+                    meshname = data[k]['DepotPath']
+            
+                    if(meshname != 0):
+                        glbfoundname = [ x for x in meshes if os.path.splitext(str(meshname))[0] in x] 
+                        if(len(glbfoundname) == 1):
+                            instances = [x for x in t if x['NodeIndex'] == i]
+                            nn = str(data['debugName']).replace('{','').replace('}','')
+                    
+                            for inst in instances:
+                                gltf_importer = glTFImporter(glbfoundname[0], { "files": None, "loglevel": 0, "import_pack_images" :True, "merge_vertices" :False, "import_shading" : 'NORMALS', "bone_heuristic":'TEMPERANCE', "guess_original_bind_pose" : False, "import_user_extensions": ""})
+                                gltf_importer.read()
+                                gltf_importer.checks()
+
+                                if self.import_materials:
+                                    existingMeshes = bpy.data.meshes.keys()
+                                    existingObjects = bpy.data.objects.keys()
+                                    existingMaterials = bpy.data.materials.keys()
+
+                                BlenderGlTF.create(gltf_importer)
+                        
+                                objects = bpy.context.selected_objects
+                                for obj in objects:                            
+                                    nn = [nn[1:],nn][nn[0].isalnum()]
+                                    obj.name = nn
+
+                                    obj.location.x = inst['Position']['Properties']['X'] /100
+                                    obj.location.y = inst['Position']['Properties']['Y'] /100
+                                    obj.location.z = inst['Position']['Properties']['Z'] /100
+
+                                    obj.rotation_quaternion.x = inst['Orientation']['Properties']['i']
+                                    obj.rotation_quaternion.y = inst['Orientation']['Properties']['j']
+                                    obj.rotation_quaternion.z = inst['Orientation']['Properties']['k']
+                                    obj.rotation_quaternion.w = inst['Orientation']['Properties']['r']
+
+                                    obj.scale.x = inst['Scale']['Properties']['X'] /100
+                                    obj.scale.y = inst['Scale']['Properties']['Y'] /100
+                                    obj.scale.z = inst['Scale']['Properties']['Z'] /100
+
+                                # TODO: move material processing out to it's own function
+                                if self.import_materials:
+                                    for name in bpy.data.materials.keys():
+                                        if name not in existingMaterials:
+                                            bpy.data.materials.remove(bpy.data.materials[name], do_unlink=True, do_id_user=True, do_ui_user=True)
+
+                                    if platform.system() == 'Windows':
+                                        # windows can reach MAX_PATH easily
+                                        BasePath = u"\\\\?\\" + os.path.splitext(glbfoundname[0])[0]
+                                    else:
+                                        BasePath = os.path.splitext(glbfoundname[0])[0]
+
+                                    file = open(BasePath + ".Material.json",mode='r')
+                                    obj2 = json.loads(file.read())
+                                    BasePath = str(obj2["MaterialRepo"])  + "\\"
+
+                                    Builder = MaterialBuilder(obj2, BasePath, str(self.image_format))
+
+                                    usedMaterials = {}
+                                    counter = 0
+                                    for name in bpy.data.meshes.keys():
+                                        if name not in existingMeshes:
+                                            bpy.data.meshes[name].materials.clear()
+                                            for matname in gltf_importer.data.meshes[counter].extras["materialNames"]:
+                                                if matname not in usedMaterials.keys():
+                                                    index = 0
+                                                    for rawmat in obj2["Materials"]:
+                                                        if rawmat["Name"] == matname:
+                                                            bpymat = Builder.create(index)
+                                                            bpy.data.meshes[name].materials.append(bpymat)
+                                                            usedMaterials.update( {matname: bpymat} )
+                                                        index = index + 1
+                                                else:
+                                                    bpy.data.meshes[name].materials.append(usedMaterials[matname])
+                        
+                                            counter = counter + 1
+
+                                    if not self.exclude_unused_mats:
+                                        index = 0
+                                        for rawmat in obj2["Materials"]:
+                                            if rawmat["Name"] not in usedMaterials:
+                                                Builder.create(index)
+                                            index = index + 1
+
+                                    collection = bpy.data.collections.new(os.path.splitext(os.path.basename(glbfoundname[0]))[0])
+                                    bpy.context.scene.collection.children.link(collection)
+
+                                    for name in bpy.data.objects.keys():
+                                        if name not in existingObjects:
+                                            for parent in bpy.data.objects[name].users_collection:
+                                                parent.objects.unlink(bpy.data.objects[name])
+                                            collection.objects.link(bpy.data.objects[name])
+
+        self.report({'INFO'}, "Streaming Sector Import successful!")
         return {'FINISHED'}
 
 class CP77Import(bpy.types.Operator,ImportHelper):
@@ -57,7 +195,7 @@ class CP77Import(bpy.types.Operator,ImportHelper):
                 ("jpeg", "Use JPEG textures", "")),
         description="Texture Format",
         default="png")
-    exclude_unused_mats: BoolProperty(name="Exclude Unused Materials",default=True,description="Enabling this options skips all the materials that aren't being used by any mesh")
+    exclude_unused_mats: BoolProperty(name="Exclude Unused Materials",default=True,description="Enabling this options skips all the materials that aren't being used by any mesh") 
     filepath: StringProperty(subtype = 'FILE_PATH')
 
     def draw(self, context):
@@ -70,7 +208,6 @@ class CP77Import(bpy.types.Operator,ImportHelper):
         gltf_importer = glTFImporter(self.filepath, { "files": None, "loglevel": 0, "import_pack_images" :True, "merge_vertices" :False, "import_shading" : 'NORMALS', "bone_heuristic":'TEMPERANCE', "guess_original_bind_pose" : False, "import_user_extensions": ""})
         gltf_importer.read()
         gltf_importer.checks()
-
 
         existingMeshes = bpy.data.meshes.keys()
         existingObjects = bpy.data.objects.keys()
@@ -129,7 +266,7 @@ class CP77Import(bpy.types.Operator,ImportHelper):
 
 def menu_func_import(self, context):
     self.layout.operator(CP77Import.bl_idname, text="Cyberpunk GLTF (.gltf/.glb)", icon_value=custom_icon_col["import"]['WKIT'].icon_id)
-    #self.layout.operator(CP77StreamingSectorImport.bl_idname, text="Cyberpunk StreamingSector (.json)")
+    self.layout.operator(CP77StreamingSectorImport.bl_idname, text="Cyberpunk StreamingSector (.json)")
 
 def register():
     custom_icon = bpy.utils.previews.new()
@@ -137,14 +274,14 @@ def register():
     custom_icon_col["import"] = custom_icon
 
     bpy.utils.register_class(CP77Import)
-    #bpy.utils.register_class(CP77StreamingSectorImport)
+    bpy.utils.register_class(CP77StreamingSectorImport)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
     
 def unregister():
     bpy.utils.previews.remove(custom_icon_col["import"])
 
     bpy.utils.unregister_class(CP77Import)
-    #bpy.utils.unregister_class(CP77StreamingSectorImport)
+    bpy.utils.unregister_class(CP77StreamingSectorImport)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
         
 if __name__ == "__main__":
--- i_scene_cp77_gltf/main/setup.py
+++ i_scene_cp77_gltf/main/setup.py
@@ -29,61 +29,88 @@ class MaterialBuilder:
         if rawMat["MaterialTemplate"] == "engine\\materials\\multilayered.mt":
             multilayered = Multilayered(self.BasePath,self.image_format)
             multilayered.create(rawMat["Data"],bpyMat)
+            return bpyMat
+
+        # multilayered terrain (needs work)
+        if rawMat["MaterialTemplate"] == "engine\\materials\\multilayered_terrain.mt":
+            multilayered = Multilayered(self.BasePath,self.image_format)
+            multilayered.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\vehicle_destr_blendshape.mt":
             vehicleDestrBlendshape = VehicleDestrBlendshape(self.BasePath,self.image_format)
             vehicleDestrBlendshape.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\mesh_decal.mt":
             meshDecal = MeshDecal(self.BasePath,self.image_format)
             meshDecal.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\mesh_decal_double_diffuse.mt":
             meshDecalDoubleDiffuse = MeshDecalDoubleDiffuse(self.BasePath,self.image_format)
             meshDecalDoubleDiffuse.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\vehicle_mesh_decal.mt":
             vehicleMeshDecal = VehicleMeshDecal(self.BasePath,self.image_format)
             vehicleMeshDecal.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\skin.mt":
             skin = Skin(self.BasePath,self.image_format)
             skin.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "engine\\materials\\metal_base.remt":
             metalBase = MetalBase(self.BasePath,self.image_format)
             metalBase.create(rawMat["Data"],bpyMat)
+            return bpyMat
+
+        # metal proxies
+        if rawMat["MaterialTemplate"] == "engine\\materials\\metal_base_proxy.mt":
+            metalBase = MetalBase(self.BasePath,self.image_format)
+            metalBase.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\hair.mt":
             hair = Hair(self.BasePath,self.image_format)
             hair.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\mesh_decal_gradientmap_recolor.mt":
             meshDecalGradientMapReColor = MeshDecalGradientMapReColor(self.BasePath,self.image_format)
             meshDecalGradientMapReColor.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\eye.mt":
             eye = Eye(self.BasePath,self.image_format)
             eye.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\eye_gradient.mt":
             eyeGradient = EyeGradient(self.BasePath,self.image_format)
             eyeGradient.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\eye_shadow.mt":
             eyeShadow = EyeShadow(self.BasePath,self.image_format)
             eyeShadow.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\mesh_decal_emissive.mt":
             meshDecalEmissive = MeshDecalEmissive(self.BasePath,self.image_format)
             meshDecalEmissive.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\mesh_decal_wet_character.mt":
             meshDecal = MeshDecal(self.BasePath,self.image_format)
             meshDecal.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         if rawMat["MaterialTemplate"] == "base\\materials\\glass.mt":
             glass = Glass(self.BasePath,self.image_format)
             glass.create(rawMat["Data"],bpyMat)
+            return bpyMat
 
         return bpyMat
